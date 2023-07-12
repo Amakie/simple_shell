1db5dce55be4609406b36d70ef22b40636649186
Constance Amatey <consamate57@gmail.com>
2023-07-12 14:10:30 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <strings.h>
+#include <stdlid.h>
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+
+
+#endif
